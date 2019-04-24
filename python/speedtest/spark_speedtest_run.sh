8baad02ceb025aac8b171f6857933391a2c2d681
Feng Li <m@feng.li>
2019-04-24 09:53:22 +0800

    Minor update

--- python/speedtest/spark_speedtest_run.sh
+++ python/speedtest/spark_speedtest_run.sh
@@ -4,9 +4,10 @@
 # https://help.aliyun.com/document_detail/28124.html
 
 spark-submit \
-    --master yarn \
-    --driver-memory 30g \
-    --executor-memory 5g \
+    --master yarn          \
+    --driver-memory 30g    \ # Master's RAM
+    --executor-memory 5g   \ # Worker's RAM
+    --num-executors 2      \
     spark_speedtest.py
 
 
