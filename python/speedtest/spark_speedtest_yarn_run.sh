d5ab39cd2dd9aa60b1b5bc221b5482facf43ffb8
Feng Li <m@feng.li>
2019-04-25 23:40:38 +0800

    Update script

---
+++ python/speedtest/spark_speedtest_yarn_run.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+# https://help.aliyun.com/document_detail/28124.html
+# Fat executors: one executor per node
+EC=16
+EM=32g
+
+# Tiny executors: one executor per core
+# EC=1
+# EM=2g
+
+for i in {1..2..1}
+do
+    spark-submit \
+        --master yarn  \
+        --driver-memory 30g    \
+        --executor-memory $EM   \
+        --num-executors $i      \
+        --executor-cores $EC
+        --conf spark.rpc.message.maxSize=1024 \
+        spark_speedtest.py   \
+        > speedtest.s$i.ec$EC 2> speedtest.s$i.ec$EC.log
+done
+
+exit 0;
