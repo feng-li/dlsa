db8b73622ffee4f4c2cd6c6a274acb7e3c5a5519
Feng Li <m@feng.li>
2019-05-18 20:52:27 +0800

    Update testings

--- python/speedtest/logistic_LBFGS.py
+++ python/speedtest/logistic_LBFGS.py
@@ -14,32 +14,61 @@ from pyspark.mllib.regression import LabeledPoint
 tic0 = time.clock()
 sc = pyspark.SparkContext("yarn", "Speed Test App")
 
-## Simulate Data
-n = 500000
+
+## Simulation settings
+save_to_hdfs = False
+
+nsub = 5
+n = 100000
 p = 500
 p1 = int(p * 0.3)
 
-features = np.random.rand(n, p) - 0.5
-
-## TRUE beta
 beta = np.zeros(p).reshape(p, 1)
 beta[:p1] = 1
-prob = 1 / (1 + np.exp(-features.dot(beta)))
 
-label = np.zeros(n).reshape(n, 1)
-for i in range(n):
-    # TODO: REMOVE loop
-    label[i] = np.random.binomial(n=1,p=prob[i], size=1)
+time_parallelize = 0
+for isub in range(nsub):
 
-df = np.concatenate((label, features), 1)
-# pdf = pd.DataFrame(df, columns=["label"] + ["x" + str(x) for x in range(p)])
-memsize = sys.getsizeof(df)
+    features = np.random.rand(n, p) - 0.5
 
-dff = map(lambda x: LabeledPoint(int(x[0]), x[1:]), df)
+    ## TRUE beta
+    prob = 1 / (1 + np.exp(-features.dot(beta)))
 
-tic = time.clock()
-parsedData = sc.parallelize(dff)
-time_parallelize = time.clock() - tic
+    label = np.zeros(n).reshape(n, 1)
+    for i in range(n):
+        # TODO: REMOVE loop
+        label[i] = np.random.binomial(n=1,p=prob[i], size=1)
+
+    df = np.concatenate((label, features), 1)
+    # pdf = pd.DataFrame(df, columns=["label"] + ["x" + str(x) for x in range(p)])
+    memsize_isub = sys.getsizeof(df)
+
+    dff = map(lambda x: LabeledPoint(int(x[0]), x[1:]), df)
+
+    tic = time.clock()
+    parsedDataNew = sc.parallelize(dff)
+
+    if isub == 0:
+        parsedData = parsedDataNew
+    else:
+        parsedData = parsedData.union(parsedDataNew)
+
+
+    ## https://spark.apache.org/docs/latest/rdd-programming-guide.html#rdd-persistence
+    ## https://spark.apache.org/docs/latest/api/python/pyspark.html#pyspark.StorageLevel
+    ## parsedDataNew.persist(pyspark.StorageLevel.MEMORY_AND_DISK)
+    parsedData.persist(pyspark.StorageLevel.DISK_ONLY)
+
+    parsedDataNew.unpersist() # delete rdd
+
+
+    time_parallelize += (time.clock() - tic)
+
+
+del dff, df, parsedDataNew
+
+if save_to_hdfs == True:
+    parsedData.saveAsTextFile("/data/simulation/")
 
 ##----------------------------------------------------------------------------------------
 ## Logistic Regression with LBFGS
@@ -52,7 +81,9 @@ time_clusterrun = time.clock() - tic
 
 time_wallclock = time.clock() - tic0
 
-out = [n, p, memsize, time_parallelize, time_clusterrun, time_wallclock]
+memsize = memsize_isub * (nsub + 1)
+
+out = [n * nsub, p, memsize, time_parallelize, time_clusterrun, time_wallclock]
 print(", ".join(format(x, "10.4f") for x in out))
 
 sc.stop()
--- python/speedtest/spark_speedtest_yarn_run.sh
+++ python/speedtest/spark_speedtest_yarn_run.sh
@@ -8,14 +8,15 @@
 
 # Tiny executors: one executor per core
 EC=1
-EM=5g
+EM=1g
 
 MODEL_FILE=logistic_LBFGS
 # MODEL_FILE=logistic_SGD
 
 # for i in 1 {4..100..4} # 1, 5, 10, 15, ... , 100
-for i in {64..4..-4}
+for i in {256..4..-4}
 do
+    tic=`date +%s`
     PYSPARK_PYTHON=python3 spark-submit \
                   --master yarn  \
                   --driver-memory 50g    \
@@ -25,7 +26,9 @@ do
                   --conf spark.rpc.message.maxSize=1024 \
                   ${MODEL_FILE}.py  \
                   > ${MODEL_FILE}.NE${i}.EC${EC}.out 2> ${MODEL_FILE}.NE${i}.EC${EC}.log
-    echo ${MODEL_FILE}.NE${i}.EC${EC} done!
+    toc=`date +%s`
+    runtime=$((toc-tic))
+    echo ${MODEL_FILE}.NE${i}.EC${EC} done, "Time used (s):," $runtime
 
 done
 
