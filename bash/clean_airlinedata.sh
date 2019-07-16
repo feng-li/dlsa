ec05a579b7275216104ef169386a9b437ad3341f
Feng Li <m@feng.li>
2019-07-16 10:17:33 +0800

    Add clean airline script

--- bash/clean_airlinedata.sh
+++ bash/clean_airlinedata.sh
@@ -9,11 +9,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for i in {1987..2007..1}
 do
 
-    if [i -eq 1987]
-    then
-        lines='NR != 0'
-    else
-        lines='NR != 1'
+    # if [ $i = '1987' ]
+    # then
+    #     lines=1
+    # else
+    #     lines=2
+    # fi
 
-    awk -F, $lines ${SOURCE}${i}.csv >> ${SOURCE}allfile.csv
+    awk -F, 'NR >= 2' ${SOURCE}${i}.csv >> ${SOURCE}allfile.csv
+    echo $i is processed
 done
+
+
+# Insert a header
+# sed -i '1 i\Year,Month,DayofMonth,DayOfWeek,DepTime,CRSDepTime,ArrTime,CRSArrTime,UniqueCarrier,FlightNum,TailNum,ActualElapsedTime,CRSElapsedTime,AirTime,ArrDelay,DepDelay,Origin,Dest,Distance,TaxiIn,TaxiOut,Cancelled,CancellationCode,Diverted,CarrierDelay,WeatherDelay,NASDelay,SecurityDelay,LateAircraftDelay' allfile.csv
