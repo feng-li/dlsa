57ce0acfb45dfd19a15c43cb7f628bff672abe6b
Feng Li <m@feng.li>
2019-07-23 02:37:26 +0800

    Add utils for dummies

--- dummies.py
+++ dummies.py
@@ -4,7 +4,7 @@ import pickle
 import pandas as pd
 import os
 
-def select_dummy_factors(pdf, dummy_columns, kep_top, replace_with="00_OTHERS"):
+def select_dummy_factors(pdf, dummy_columns, keep_top, replace_with="00_OTHERS", pickle_file):
     '''Merge dummy key with frequency in the given file
 
     '''
@@ -27,7 +27,7 @@ def select_dummy_factors(pdf, dummy_columns, kep_top, replace_with="00_OTHERS"):
         if len(factor_dropped[dummy_columns[i]]) == 0:
             factor_new = []
         else:
-            factor_new = replace_with
+            factor_new = [replace_with]
         factor_new.extend(factor_selected[dummy_columns[i]])
 
         factor_selected_names[dummy_columns[i]] = [dummy_columns[i] + '_' + str(x) for x in factor_new]
@@ -38,17 +38,18 @@ def select_dummy_factors(pdf, dummy_columns, kep_top, replace_with="00_OTHERS"):
                   'factor_dropped': factor_dropped,
                   'factor_selected_names': factor_selected_names}
 
-    pickle.dump(dummy_info, open(os.path.expanduser('~/running/data_raw/dummy_info.pkl'), 'wb'))
+    pickle.dump(dummy_info, open(os.path.expanduser(pickle_file), 'wb'))
 
     return dummy_info
 
 
-pdf = pd.read_csv(os.path.expanduser("~/running/data_raw/allfile.csv.bz2"),
-                  error_bad_lines=False,
-                  usecols = [1, 3, 8, 16, 17],
-                  engine='c') # The C engine is faster
-dummy_info = select_dummy_factors(pdf,
-                                  dummy_columns=['Month', 'DayOfWeek', 'UniqueCarrier', 'Origin', 'Dest'],
-                                  kep_top= [1, 1, 0.9, 0.9, 0.9],
-                                  replace_with='00_OTHERS'
-)
+# pdf = pd.read_csv(os.path.expanduser("~/running/data_raw/dummies.csv.bz2"),
+#                   error_bad_lines=False,
+#                   # usecols = [1, 3, 8, 16, 17],
+#                   engine='c') # The C engine is faster
+# dummy_info = select_dummy_factors(pdf,
+#                                   dummy_columns=['Month', 'DayOfWeek', 'UniqueCarrier', 'Origin', 'Dest'],
+#                                   keep_top= [1, 1, 0.9, 0.9, 0.9],
+#                                   replace_with='00_OTHERS',
+#                                   pickle_file='~/running/data_raw/dummy_info.pkl'
+# )
