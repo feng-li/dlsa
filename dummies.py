de9ef5950b03fe4a765bfe7156a2f530b4f9292f
Feng Li <m@feng.li>
2019-07-23 01:36:10 +0800

    Add utils for dummies

--- dummies.py
+++ dummies.py
@@ -4,7 +4,7 @@ import pickle
 import pandas as pd
 import os
 
-def select_dummy_factors(pdf, dummy_columns, kep_top):
+def select_dummy_factors(pdf, dummy_columns, kep_top, replace_with="00_OTHERS"):
     '''Merge dummy key with frequency in the given file
 
     '''
@@ -13,7 +13,7 @@ def select_dummy_factors(pdf, dummy_columns, kep_top):
     factor_set = {}
     factor_selected = {}
     factor_dropped = {}
-    factor_selected_names = []
+    factor_selected_names = {}
 
     for i in range(len(dummy_columns)):
 
@@ -23,7 +23,14 @@ def select_dummy_factors(pdf, dummy_columns, kep_top):
         factor_selected[dummy_columns[i]] = sorted(list(factor_counts.index[factor_cum <= keep_top[i]]))
         factor_dropped[dummy_columns[i]] = sorted(list(factor_counts.index[factor_cum > keep_top[i]]))
         factor_set[dummy_columns[i]] = sorted(list(factor_counts.index))
-        factor_selected_names.extend([dummy_columns[i] + '_' + str(x) for x in factor_selected[dummy_columns[i]]])
+
+        if len(factor_dropped[dummy_columns[i]]) == 0:
+            factor_new = []
+        else:
+            factor_new = replace_with
+        factor_new.extend(factor_selected[dummy_columns[i]])
+
+        factor_selected_names[dummy_columns[i]] = [dummy_columns[i] + '_' + str(x) for x in factor_new]
 
 
     dummy_info = {'factor_set': factor_set,
@@ -36,11 +43,12 @@ def select_dummy_factors(pdf, dummy_columns, kep_top):
     return dummy_info
 
 
-# pdf = pd.read_csv(os.path.expanduser("~/running/data_raw/allfile.csv.bz2"),
-#                   error_bad_lines=False,
-#                   usecols = [1,2,3,4,5,6,7,8,11,13,14,15,16,17,18],
-#                   engine='c') # The C engine is faster
-# dummy_info = select_dummy_factors(pdf,
-#                                   dummy_columns=['Month', 'DayOfWeek', 'UniqueCarrier', 'Origin', 'Dest'],
-#                                   kep_top= [1, 1, 0.95, 0.95, 0.95])
-#
+pdf = pd.read_csv(os.path.expanduser("~/running/data_raw/allfile.csv.bz2"),
+                  error_bad_lines=False,
+                  usecols = [1, 3, 8, 16, 17],
+                  engine='c') # The C engine is faster
+dummy_info = select_dummy_factors(pdf,
+                                  dummy_columns=['Month', 'DayOfWeek', 'UniqueCarrier', 'Origin', 'Dest'],
+                                  kep_top= [1, 1, 0.9, 0.9, 0.9],
+                                  replace_with='00_OTHERS'
+)
