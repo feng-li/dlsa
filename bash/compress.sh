036cf300708092d6acaf2c10eb73ed92f8e626c5
Feng Li <m@feng.li>
2019-07-23 22:10:36 +0800

    Add compress script

---
+++ bash/compress.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+for file in xa*.csv
+do
+
+    # sed -i '1 i\Year,Month,DayofMonth,DayOfWeek,DepTime,CRSDepTime,ArrTime,CRSArrTime,UniqueCarrier,FlightNum,TailNum,ActualElapsedTime,CRSElapsedTime,AirTime,ArrDelay,DepDelay,Origin,Dest,Distance,TaxiIn,TaxiOut,Cancelled,CancellationCode,Diverted,CarrierDelay,WeatherDelay,NASDelay,SecurityDelay,LateAircraftDelay' $file
+
+    bzip2 $file
+
+done
