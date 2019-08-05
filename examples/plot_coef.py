e2049ba9859b623ac33060c64fe8f015cd96c37f
Feng Li <m@feng.li>
2019-08-05 22:31:24 +0800

    Add par matrix

--- examples/plot_coef.py
+++ examples/plot_coef.py
@@ -17,12 +17,35 @@ est_sgd_key = list(est_sgd[1])
 est_sgd_idx = sorted(range(len(est_sgd_key)), key=est_sgd_key.__getitem__)
 est_dlsa_idx = sorted(range(len(est_dlsa_key)), key=est_dlsa_key.__getitem__)
 
+beta_byMLE0 = (est_sgd[0].coef_.T)
+if est_sgd[0].fit_intercept:
+    beta_byMLE0 = np.concatenate([est_sgd[0].intercept_.reshape(1, 1), beta_byMLE0])
+    est_sgd_key.insert(0, 'intercept')
 
-matrix = np.concatenate([(est_sgd[0].coef_.T)[est_sgd_idx], # GLOBAL SGD
-                         est_dlsa[0].iloc[est_dlsa_idx, 0:2], # WLSE, ONE_HOT
-                         est_dlsa[1].iloc[est_dlsa_idx, :]],
+beta_byMLE = beta_byMLE0.copy()
+count = 0
+for key in est_dlsa_key:
+    idx = est_sgd_key.index(key)
+    beta_byMLE[count] = beta_byMLE0[idx]
+    count += 1
+
+# MLE = pd.DataFrame({'key': np.array(est_sgd_key), 'beta_byMLE': beta_byMLE})
+
+
+# out = pd.DataFrame({
+#     'MLE': beta_byMLE, # SGD
+#     'WLSE': est_dlsa[0].iloc[:, 0],
+#     'ONESHOT': est_dlsa[0].iloc[:, 1],
+#     'DLSA_AIC': est_dlsa[1].iloc[:, 0],
+#     'DLSA_BIC': est_dlsa[1].iloc[:, 1]
+# })
+
+matrix = np.concatenate([beta_byMLE, # GLOBAL SGD
+                         # est_dlsa[0].iloc[:, 0:2], # WLSE, ONE_HOT
+                         est_dlsa[1]],
                         axis=1)
 
 
-out = pd.DataFrame(matrix, index=sorted(est_dlsa_key), columns=['MLE', 'WLSE', 'ONE_HOT', 'DLSA_AIC', 'DLSA_BIC'])
+out = pd.DataFrame(matrix, index=est_dlsa_key,
+                   columns=['MLE',  'DLSA_AIC', 'DLSA_BIC', 'WLSE', 'ONE_SHOT'])
 out.to_csv("coef.csv", index_label='Var')
