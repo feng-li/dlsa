e962691cd9ab6bb6a20c4482e9ba0882b1f411f6
Feng Li <m@feng.li>
2020-11-13 00:43:14 +0800

    minor update

--- dlsa/sdummies.py
+++ dlsa/sdummies.py
@@ -1,5 +1,5 @@
 import pyspark as ps
-from pyspark.sql import SQLContext, Column
+from pyspark.sql import Column
 from pyspark.sql.functions import col, count as sparkcount, when, lit
 from pyspark.sql.types import StringType
 from pyspark.ml.feature import StringIndexer, IndexToString
@@ -7,10 +7,13 @@ from pyspark.ml import Pipeline
 
 from pyspark.sql.functions import col
 
+
 def withMeta(self, alias, meta):
     sc = ps.SparkContext._active_spark_context
     jmeta = sc._gateway.jvm.org.apache.spark.sql.types.Metadata
-    return Column(getattr(self._jc, "as")(alias, jmeta.fromJson(json.dumps(meta))))
+    return Column(
+        getattr(self._jc, "as")(alias, jmeta.fromJson(json.dumps(meta))))
+
 
 def group_low_freq(sdf, dummy_columns, keep_top=.01, replace_with='other'):
     """
@@ -24,21 +27,30 @@ def group_low_freq(sdf, dummy_columns, keep_top=.01, replace_with='other'):
     total = sdf.count()
     for string_col in dummy_columns:
         # Apply string indexer
-        pipeline = Pipeline(stages=[StringIndexer(inputCol=string_col, outputCol="ix_"+string_col)])
+        pipeline = Pipeline(stages=[
+            StringIndexer(inputCol=string_col, outputCol="ix_" + string_col)
+        ])
         sdf = pipeline.fit(sdf).transform(sdf)
 
         # Calculate the number of unique elements to keep
-        n_to_keep = sdf.groupby(string_col).agg((sparkcount(string_col)/total).alias('perc')).filter(col('perc')>keep_top).count()
+        n_to_keep = sdf.groupby(string_col).agg(
+            (sparkcount(string_col) /
+             total).alias('perc')).filter(col('perc') > keep_top).count()
 
         # If elements occur below (keep_top * number of rows), replace them with n_to_keep.
         this_meta = sdf.select('ix_' + string_col).schema.fields[0].metadata
         if n_to_keep != len(this_meta['ml_attr']['vals']):
-            this_meta['ml_attr']['vals'] = this_meta['ml_attr']['vals'][0:(n_to_keep+1)]
+            this_meta['ml_attr']['vals'] = this_meta['ml_attr']['vals'][0:(
+                n_to_keep + 1)]
             this_meta['ml_attr']['vals'][n_to_keep] = replace_with
-            sdf = sdf.withColumn('ix_'+string_col,when(col('ix_'+string_col)>=n_to_keep,lit(n_to_keep)).otherwise(col('ix_'+string_col)))
+            sdf = sdf.withColumn(
+                'ix_' + string_col,
+                when(col('ix_' + string_col) >= n_to_keep,
+                     lit(n_to_keep)).otherwise(col('ix_' + string_col)))
 
         # add the new column with correct metadata, remove original.
-        sdf = sdf.withColumn('ix_'+string_col, withMeta(col('ix_'+string_col), "", this_meta))
+        sdf = sdf.withColumn('ix_' + string_col,
+                             withMeta(col('ix_' + string_col), "", this_meta))
 
     return sdf
 
@@ -46,19 +58,22 @@ def group_low_freq(sdf, dummy_columns, keep_top=.01, replace_with='other'):
 if __name__ == "__main__":
 
     # SAMPLE DATA -----------------------------------------------------------------
-    import numpy as np
-    df = pd.DataFrame({'x1' : ['a','b','a','b','c'],  # a: 0.4, b: 0.4, c: 0.2
-                       'x2' : ['a','b','a','b','a'],  # a: 0.6, b: 0.4, c: 0.0
-                       'x3' : ['a','a','a','a','a'],  # a: 1.0, b: 0.0, c: 0.0
-                       'x4' : ['a','b','c','d','e']}) # a: 0.2, b: 0.2, c: 0.2, d: 0.2, e: 0.2
-    sdf = sqlContext.createDataFrame(df)
+    import pandas as pd
+    df = pd.DataFrame({
+        'x1': ['a', 'b', 'a', 'b', 'c'],  # a: 0.4, b: 0.4, c: 0.2
+        'x2': ['a', 'b', 'a', 'b', 'a'],  # a: 0.6, b: 0.4, c: 0.0
+        'x3': ['a', 'a', 'a', 'a', 'a'],  # a: 1.0, b: 0.0, c: 0.0
+        'x4': ['a', 'b', 'c', 'd', 'e']
+    })  # a: 0.2, b: 0.2, c: 0.2, d: 0.2, e: 0.2
+    sdf = sc.createDataFrame(df)
 
     # TEST THE FUNCTION -----------------------------------------------------------
-    sdf = group_low_freq(sdf,sdf.columns,0.25)
+    sdf = group_low_freq(sdf, sdf.columns, 0.25)
 
     ix_cols = [x for x in sdf.columns if 'ix_' in x]
     for string_col in ix_cols:
-        idx_to_string = IndexToString(inputCol=string_col, outputCol=string_col[3:]+'grouped')
+        idx_to_string = IndexToString(inputCol=string_col,
+                                      outputCol=string_col[3:] + 'grouped')
         sdf = idx_to_string.transform(sdf)
 
     sdf.show()
