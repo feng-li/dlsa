1201ddad73c4d2dda604e5b7bb86204083781633
Feng Li <m@feng.li>
2023-10-23 11:22:44 +0800

    Update old requirements

--- projects/results/logistic_single.py
+++ projects/results/logistic_single.py
@@ -11,7 +11,7 @@ pandas_df = pd.read_csv("../data/games-expand.csv")
 # run the model on the partitioned data set
 x_train = pandas_df.drop(['label'], axis=1)
 y_train = pandas_df["label"]
-model = LogisticRegression(solver="lbfgs", fit_intercept=False, penalty=’none’)
+model = LogisticRegression(solver="lbfgs", fit_intercept=False, penalty='none')
 model.fit(x_train, y_train)
 p = model.coef_.size
 par_id = ["p" + str(x) for x in range(p)]
